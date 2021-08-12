295b76395afe63d69f2e14c82937fd2aafca1fd1
Dmitry Zaguta <zdvbind@gmail.com>
2021-08-12 22:02:40 +0500

    fix review

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe AnswersController, type: :controller do
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
-          .to change(Answer, :count).by(1)
+          .to change(question.answers, :count).by(1)
       end
 
       it 'redirect to show view' do
