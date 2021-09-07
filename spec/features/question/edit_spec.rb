6e4593f9f354df2cf74f774cded503d971804f3f
Dmitry Zaguta <zdvbind@gmail.com>
2021-09-07 16:54:34 +0500

    Author can add files when editing his question

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -43,6 +43,18 @@ feature 'User can edit his question', "
         expect(page).to have_content "Body can't be blank"
       end
     end
+
+    scenario 'edits his question and add files ', js: true do
+      within "#question-#{question.id}" do
+        fill_in 'Title', with: 'Edited title'
+        fill_in 'Your question', with: 'Edited question'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   describe 'Not author' do
