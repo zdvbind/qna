4d650ef376ebe8df7f4d330ec9aa78c574d74145
Dmitry Zaguta <zdvbind@gmail.com>
2021-09-08 12:34:19 +0500

    add feature test for delete attachments from the question

--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -8,7 +8,7 @@ feature 'Author can delete his question', "
   given(:user_author) { create(:user) }
   given(:user) { create(:user) }
   given(:question) { create(:question, author: user_author) }
-
+  given(:question_with_attachments) { create(:question, :with_attachments, author: user_author) }
   scenario 'Author can delete his question' do
     sign_in(user_author)
     visit question_path(question)
@@ -25,5 +25,20 @@ feature 'Author can delete his question', "
   scenario 'Unauthenticated user try delete a question' do
     visit question_path(question)
     expect(page).not_to have_content('Delete the question')
+  end
+
+  scenario 'Author can delete attachment', js: true do
+    sign_in(user_author)
+    visit question_path(question_with_attachments)
+    click_on 'Delete the file'
+    expect(page).to_not have_link 'rails_helper.rb'
+  end
+
+  scenario 'Not author try delete attachment', js: true do
+    sign_in(user)
+    visit question_path(question_with_attachments)
+
+    expect(page).to have_link 'rails_helper.rb'
+    expect(page).to_not have_link 'Delete the file'
   end
 end
