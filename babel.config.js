dfcbd299e845edecc96f6e39280c4db1ee2a1c68
Dmitry Zaguta <zdvbind@gmail.com>
2022-11-24 20:50:26 +0500

    fix

--- babel.config.js
+++ babel.config.js
@@ -64,6 +64,11 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      ['@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
       ]
     ].filter(Boolean)
   }
