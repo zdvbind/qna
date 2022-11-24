b8376a05cdaf75782cc2f6ac2ccec7560a797c77
Dmitry Zaguta <zdvbind@gmail.com>
2022-11-24 18:47:20 +0500

    small fix in ip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: '194.58.107.128' }
+  config.action_mailer.default_url_options = { host: '194.58.107.125' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
