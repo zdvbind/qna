a484f39c3420a0af69f97701e226ac820559b579
Dmitry Zaguta <zdvbind@gmail.com>
2022-11-28 13:14:01 +0500

    config files for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.17.1'
+
+set :application, 'qna'
+set :repo_url, 'git@github.com:zdvbind/qna.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/qna'
+set :user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/master.key'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system',
+       'vendor', 'storage'
+
+set :pty, false
